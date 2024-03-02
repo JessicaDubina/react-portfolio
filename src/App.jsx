a95aee57e97c51b13493f6fa048cb5f910e0e31e
jdub <jessicaclaing@gmail.com>
2024-03-01 17:50:04 -0800

    added nav component and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-// import HelloReact from './components/HelloReact';
+import './App.css';
+import Nav from './components/Nav';
 
 function App() {
-//   return <HelloReact />;
+    return (
+        <div>
+          <Nav />
+        </div>
+      );
 }
 
 export default App;
