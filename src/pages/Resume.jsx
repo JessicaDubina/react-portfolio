1e27adcd2a14c2db7c190012c4375ea16c470fd8
jdub <jessicaclaing@gmail.com>
2024-03-04 23:33:51 -0800

    changed display for pdf to iframe instead of react-pdf. removed unused dependancies from package.json

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,27 +1,23 @@
 import resume from '../assets/resume.pdf';
-import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import React from 'react';
 
 export default function Resume() {
   const styles = {
     font: {
       fontSize: 'calc(1.75rem + 1.25vw)'
+    },
+    pdf: {
+   
+      height: '800px'
     }
   }  
   
   return (
       <div>
         <h1 className="d-flex justify-content-center" style={styles.font}>Resume</h1>
-        <p>
-          Add in downloadable resume here
-        </p>
-        <Document
-          file={resume}
-          onLoadError={console.error}
-          style={{ width: "100vw", height: 'auto'}}
-        >
-          <Page pageIndex={0}/>
-        </Document>
+        <div className='card col-md-8 my-3 mx-auto border p-2 shadow' style={styles.pdf}>
+          <iframe src={resume} style={styles.pdf}/>
+        </div>
       </div>
     );
   }
