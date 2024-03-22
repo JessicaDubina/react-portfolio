04724409f2869327d68b45a23527126db102b526
jdub <jessicaclaing@gmail.com>
2024-03-22 08:21:12 -0700

    added exam builder project to portfolio

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -4,20 +4,29 @@ import password from './password.png'
 import note from './note-taker.png'
 import weather from './weather.png'
 import scheduler from './scheduler.png'
+import exam from './exam-builder.png'
 
 const projectsData = 
 
 [
     {
         "projectId": 1,
+        "title": "Exam Builder",
+        "deployed": "https://exam-builder.onrender.com/",
+        "github": "https://github.com/JessicaDubina/exam-builder",
+        "image": exam,
+        "bodyBackground": "white"
+    },
+    {
+        "projectId": 2,
         "title": "Gnome Rouges",
-        "deployed": "testhttps://gnome-rogues-90a0baf68228.herokuapp.com/",
+        "deployed": "https://gnome-rogues-90a0baf68228.herokuapp.com/",
         "github": "https://github.com/mcgintyb14/Gnome-Rogues",
         "image": gnome,
         "bodyBackground": "rgb(186, 222, 235)"
     },
     {
-        "projectId": 2,
+        "projectId": 3,
         "title": "ChuckYeezy Meme Generator",
         "deployed": "https://jessicadubina.github.io/ChuckYe-Meme-Generator/",
         "github": "https://github.com/JessicaDubina/ChuckYe-Meme-Generator",
@@ -25,7 +34,7 @@ const projectsData =
         "bodyBackground": "white"
     },
     {
-        "projectId": 3,
+        "projectId": 4,
         "title": "Note Taking App",
         "deployed": "https://jessnotetaker-802871438d82.herokuapp.com/",
         "github": "https://github.com/JessicaDubina/note-taker",
@@ -33,7 +42,7 @@ const projectsData =
         "bodyBackground": "white"
     },
     {
-        "projectId": 4,
+        "projectId": 5,
         "title": "Weather Dashboard",
         "deployed": "https://jessicadubina.github.io/weather-dashboard/",
         "github": "https://github.com/JessicaDubina/weather-dashboard",
@@ -41,7 +50,7 @@ const projectsData =
         "bodyBackground": "white"
     },
     {
-        "projectId": 5,
+        "projectId": 6,
         "title": "Workday Planner",
         "deployed": "https://jessicadubina.github.io/work-day-scheduler/",
         "github": "https://github.com/JessicaDubina/work-day-scheduler",
@@ -49,7 +58,7 @@ const projectsData =
         "bodyBackground": "white"
     },
     {
-        "projectId": 6,
+        "projectId": 7,
         "title": "Password Generator",
         "deployed": "https://jessicadubina.github.io/password-generator/",
         "github": "https://github.com/JessicaDubina/password-generator",
